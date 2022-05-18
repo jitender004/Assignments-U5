506e37412e2c32cd04a7bf62958c31b5b5502979
jitender004 <jitenderjattu04@gmail.com>
2022-05-19 01:15:30 +0530

    todo changed

--- react-todo/src/components/Todo.jsx
+++ react-todo/src/components/Todo.jsx
@@ -1,19 +1,45 @@
-import React, {useState} from 'react'
-import TodoList from './TodoList';
+import React, { useState } from "react";
+import TaskItem from "./TaskItem";
+// import TodoList from './TodoList';
+
 //read input
 const Todo = () => {
-    const [todo,setTodo] = useState("");
-    const handleChange = (e)=>{
-    //    console.log(e.target.value);
-       setTodo(e.target.value);
-    }
-    
-  return (
-    <div>
-        <input onChange={handleChange} value={todo} type="text" placeholder='Write Something' />   
-         <TodoList />
-    </div>
-  )
-}
+  const [todo, setTodo] = useState("");
+  const [task, setTask] = useState([]);
+  const handleChange = (e) => {
+    // console.log(e.target.value);
+    setTodo(e.target.value);
+  };
 
-export default Todo
+  const handleClick = () => {
+    const payload = {
+      title: todo,
+      status: false,
+    };
+    const newTask = [...task, payload];
+    setTask(newTask);
+    setTodo("");
+  };
+
+  return (
+    <>
+      <div>
+        <h1>Todo-App</h1>
+        <input
+          type="text"
+          value={todo}
+          onChange={handleChange}
+          placeholder="Add Something here!"
+        />
+        <button onClick={handleClick}>Add</button>
+      </div>
+      <div>
+        {task.map((item) => {
+          return <TaskItem id={item.id} title={item.title} />;
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Todo;
